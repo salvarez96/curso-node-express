87eaf487e580be8d367d30191ae9b7f9b1f27023
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-10 12:51:50 -0500

    ➕ Install express and launch basic server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hola, mi server en express')
+})
+
+app.listen(port, () => {
+  console.log(`Inicia la aplicación en el puerto ${port}`);
+})
