dd059013552aec47f7090935442e137ab7e45f04
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-20 00:45:12 -0500

    🚚 Create api/v1 main path to encapsulate all APIs

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,12 @@
+const express = require('express');
 const productsRouter = require('@products/products.routes');
 
 function routerApi(app) {
-  app.use('/api/products', productsRouter)
+  // configuring the main endpoint /api/v1:
+  const v1Router = express.Router()
+  app.use('/api/v1', v1Router)
+
+  v1Router.use('/products', productsRouter)
 }
 
 module.exports = routerApi
