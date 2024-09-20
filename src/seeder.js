1b9ef9b04898ae648e9faf5f168d35ca4952a846
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-19 22:12:07 -0500

    ⚰️ Remove previous helpers from seeder file

--- src/seeder.js
+++ src/seeder.js
@@ -1,11 +1,6 @@
 require('module-alias/register')
-const { createDataFile } = require('./helpers/createDataFile');
-const { createFakeProducts } = require('@products/products.seeder');
-
+const { createFakeProducts } = require('@products/products.seeder')
 
 createFakeProducts()
   .then(( res ) => console.log(res))
   .catch(( err ) => console.error(err))
-// createDataFile('products.json')
-//     .then((message) => console.log(message))
-//     .catch((err) => console.error(err))
