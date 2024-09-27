59ee102e556db447feabfe3eeb64927a65bc552c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-09-27 17:49:25 -0500

    👔 Create new products.service
    Create new products.service to handle product's business logic.

---
+++ src/services/products/products.service.js
@@ -0,0 +1,114 @@
+const { getDataPath } = require('@helpers/getDataPath')
+const { DataFileHandler } = require('../../helpers/dataFileHandler')
+const { faker } = require('@faker-js/faker');
+
+class ProductsService {
+
+  constructor() {
+    this.productsPath = getDataPath('products')
+    this.products = this.getProducts()
+  }
+
+  async getProducts () {
+    try {
+      return this.products = await DataFileHandler.readDataFile(this.productsPath)
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async create(product) {
+    const cleanBody = {
+      'productName': product.productName,
+      'productPrice': product.productPrice,
+      'productImage': faker.image.url()
+    }
+
+    try {
+      this.products.metadata.lastItemId += 1
+      this.products.metadata.totalItems += 1
+
+      product = { id: this.products.metadata.lastItemId, ...cleanBody }
+
+      this.products.data.push(product)
+
+      await DataFileHandler.writeDataFile('products', this.products, 'New product')
+
+      return product
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async find(productId = undefined) {
+    try {
+      productId = productId ? productId * 1 : undefined
+      if (typeof productId === 'number' && productId >= 0) {
+        const product = await this.products.data.find((product) => {
+          return product.id == productId
+        })
+
+        return product
+      } else if (productId === undefined) {
+        return this.products
+      }
+      return false
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async update(productId, updatedContent, acceptedPropertyList) {
+    try {
+      const product = this.products.data.find(product => product.id == productId)
+      const productKey = this.products.data.findIndex(product => product.id == productId)
+
+      const acceptedNewProperties = {}
+
+      acceptedPropertyList.forEach(property => {
+        if (updatedContent[property]) {
+          acceptedNewProperties[property] = updatedContent[property]
+        }
+      })
+
+      if (!Object.keys(acceptedNewProperties).length) {
+        return null
+      }
+
+      const updatedProduct = {...product, ...acceptedNewProperties}
+
+      this.products.data[productKey] = updatedProduct
+
+      const dataWriteResponse = await DataFileHandler.writeDataFile('products', this.products, 'Product update')
+      console.log(dataWriteResponse);
+
+      return updatedProduct
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async delete(productId) {
+    try {
+      const productIndex = this.products.data.findIndex(product => product.id == productId)
+
+      if (productIndex < 0) {
+        return false
+      }
+
+      const deletedProduct = this.products.data.splice(productIndex, 1)
+      this.products.metadata.totalItems -= 1
+
+      const dataWriteResponse = await DataFileHandler.writeDataFile('products', this.products, 'Product delete')
+      console.log(dataWriteResponse);
+
+      return deletedProduct
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = {
+  ProductsService
+}
