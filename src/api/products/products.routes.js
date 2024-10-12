9e297a3f941a454f2ddd84fa80ce03d5429447b0
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-10-11 19:29:51 -0500

    🥅 Use new middlewares to catch errors in products API
    Use new middlewares to handle application errors and a combination of middlewares and boom library to handle client errors

--- src/api/products/products.routes.js
+++ src/api/products/products.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { ProductsService } = require('@services/products/products.service')
+const boom = require('@hapi/boom');
 
 const router = Router()
 const productsService = new ProductsService()
 // ruta para enviar todos los productos al front
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const { size } = req.query
     const productsList = await productsService.find()
@@ -27,14 +28,14 @@ router.get('/', async (req, res) => {
         res
           .status(200)
           .json({
-            code: 200,
+            statusCode: 200,
             data: filteredProducts
           })
       } else {
         res
           .status(204)
           .json({
-            code: 204,
+            statusCode: 204,
             message: `No products in the list.`
           })
       }
@@ -42,156 +43,103 @@ router.get('/', async (req, res) => {
       res
         .status(200)
         .json({
-          code: 200,
+          statusCode: 200,
           message: 'Products found',
           totalProducts: await productsList.metadata.totalItems,
           data: await productsList.data,
         })
     }
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        code: 500,
-        message: 'Error retrieving products:', err
-      })
-    throw err
+    next(err)
   }
 })
 
 // ruta para filtrar productos por id
-router.get('/:productId', async (req, res) => {
-  const { productId } = req.params
-
+router.get('/:productId', async (req, res, next) => {
   try {
+    const { productId } = req.params
     const product = await productsService.find(productId)
 
     if (product) {
       res
         .status(200)
         .json({
-          code: 200,
+          statusCode: 200,
           data: await product
         })
     } else {
-      res
-        .status(404)
-        .json({
-          code: 404,
-          message: `No product with id: ${productId}`
-        })
+      throw boom.notFound(`No product with id: ${productId}`)
     }
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        code: 500,
-        message: `Error getting product with id: ${productId}`, err
-      })
-    throw err
+    next(err)
   }
 })
 
-router.post('/', async (req, res) => {
-  const body = req.body
-  const propertyList = ['productName', 'productPrice']
-
-  const propertiesNotIncluded = []
-
-  propertyList.forEach(property => {
-    if (!body[property]) {
-      propertiesNotIncluded.push(property)
-    }
-  })
-
-  if (propertiesNotIncluded.length) {
-    return res
-      .status(400)
-      .json({
-        status: 400,
-        message: `The following properties are missing from the body: ${propertiesNotIncluded}`
-      })
-  }
-
+router.post('/', async (req, res, next) => {
   try {
+    const body = req.body
+    const propertyList = ['productName', 'productPrice']
+    const propertiesNotIncluded = []
+
+    propertyList.forEach(property => {
+      if (!body[property]) {
+        propertiesNotIncluded.push(property)
+      }
+    })
+
+    if (propertiesNotIncluded.length) {
+      throw boom.badRequest(`The following properties are missing from the body: ${propertiesNotIncluded}`)
+    }
+
     const newProduct = await productsService.create(body)
 
     return res
       .status(201)
       .json({
-        status: 201,
+        statusCode: 201,
         message: "Product created successfully",
         data: await newProduct
       })
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        code: 500,
-        message: `Error creating new product`, err
-      })
-    throw err
+    next(err)
   }
 })
 
-router.patch('/:productId', async (req, res) => {
-  const { productId } = req.params
-  const body = req.body
-  const acceptedPropertyList = ['productName', 'productPrice', 'productImage']
-
+router.patch('/:productId', async (req, res, next) => {
   try {
+    const { productId } = req.params
+    const body = req.body
+    const acceptedPropertyList = ['productName', 'productPrice', 'productImage']
     const product = await productsService.update(productId, body, acceptedPropertyList)
 
     if (!product) {
       switch (product) {
         case null:
-          return res
-            .status(400)
-            .json({
-              code: 400,
-              message: `Object with id ${productId} hasn't been updated due to unchanges in its specific properties.`
-            })
+          throw boom.badRequest(`Object with id ${productId} hasn't been updated due to unchanges in its specific properties.`)
         case false:
-          return res
-            .status(404)
-            .json({
-              code: 404,
-              message: `The product with id ${productId} doesn't exist.`
-            })
+          throw boom.notFound(`The product with id ${productId} doesn't exist.`)
       }
     }
 
     return res
       .status(202)
       .json({
-        status: 202,
+        statusCode: 202,
         message: "Product updated successfully",
         data: await product
       })
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        code: 500,
-        message: `Error updating product with id ${productId}:`, err
-      })
-    throw err
+    next(err)
   }
 })
 
-router.delete('/:productId', async (req, res) => {
-  const { productId }= req.params
-
+router.delete('/:productId', async (req, res, next) => {
   try {
+    const { productId }= req.params
     const product = await productsService.delete(productId)
 
     if (!product) {
-      return res
-          .status(404)
-          .json({
-            code: 404,
-            message: `Unable to delete product with id: ${productId}. The product doesn't exist.`
-          })
+      throw boom.notFound(`Unable to delete product with id: ${productId}. The product doesn't exist.`)
     }
 
     return res
@@ -203,13 +151,7 @@ router.delete('/:productId', async (req, res) => {
       })
 
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        code: 500,
-        message: `Error deleting product with id: ${productId},`, err
-      })
-    throw err
+    next(err)
   }
 })
 
