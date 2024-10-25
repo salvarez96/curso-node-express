6070b3abce548d2630abe10a552e238e566a4c9a
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-10-25 16:41:17 -0500

    🦺 Create users and categories schemas

---
+++ src/api/categories/categories.schema.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer()
+const categoryName = Joi.string().min(3).max(20)
+const categoryDescription = Joi.number().positive().min(1)
+const categoryImage = Joi.string().uri()
+
+const createCategorySchema = Joi.object({
+  categoryName: categoryName.required(),
+  categoryDescription: categoryDescription.required(),
+  categoryImage: categoryImage
+})
+
+const updateCategorySchema = Joi.object({
+  categoryName: categoryName,
+  categoryDescription: categoryDescription,
+  categoryImage: categoryImage
+})
+
+const getCategorySchema = Joi.object({
+  id: id.required()
+})
+
+const deleteCategorySchema = getCategorySchema
+
+module.exports = {
+  createCategorySchema,
+  updateCategorySchema,
+  getCategorySchema,
+  deleteCategorySchema
+}
---
+++ src/api/users/users.schema.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer()
+const userName = Joi.string().min(3).max(20)
+const userAge = Joi.number().positive().min(18).max(90)
+const userIdentificationNumber = Joi.integer()
+const userBirthDate = Joi.date().max(`${2024 - 18}`)
+const userBirthCountry = Joi.string()
+
+const createUserSchema = Joi.object({
+  userName: userName.required(),
+  userAge: userAge.required(),
+  userIdentificationNumber: userIdentificationNumber.required(),
+  userBirthDate: userBirthDate.required(),
+  userBirthCountry: userBirthCountry.required()
+})
+
+const updateUserSchema = Joi.object({
+  userName: userName,
+  userAge: userAge,
+  userIdentificationNumber: userIdentificationNumber,
+  userBirthDate: userBirthDate,
+  userBirthCountry: userBirthCountry
+})
+
+const getUserSchema = Joi.object({
+  id: id.required()
+})
+
+const deleteUserSchema = getUserSchema
+
+module.exports = {
+  createUserSchema,
+  updateUserSchema,
+  getUserSchema,
+  deleteUserSchema
+}
