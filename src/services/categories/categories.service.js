74f4b0f74797ad9e2ec93588d5538a4149337e5b
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-10-25 18:02:02 -0500

    👔 Create users and categories services

---
+++ src/services/categories/categories.service.js
@@ -0,0 +1,110 @@
+const { getDataPath } = require('@helpers/getDataPath')
+const { DataFileHandler } = require('../../helpers/dataFileHandler')
+const { faker } = require('@faker-js/faker');
+
+class CategoriesService {
+
+  constructor() {
+    this.categoriesPath = getDataPath('categories')
+  }
+
+  async getCategories () {
+    try {
+      return await DataFileHandler.readDataFile(this.categoriesPath)
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async create(category) {
+    const cleanBody = {
+      'categoryName': category.categoryName,
+      'categoryDescription': category.categoryDescription,
+      'categoryImage': faker.image.url()
+    }
+
+    try {
+      const { data, metadata } = await this.getCategories()
+
+      metadata.lastItemId++
+      metadata.totalItems++
+
+      category = { id: metadata.lastItemId, ...cleanBody }
+
+      data.push(category)
+
+      await DataFileHandler.writeDataFile('categories', { data, metadata }, 'New category')
+
+      return category
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async find(categoryId = undefined) {
+    try {
+      const categories = await this.getCategories()
+
+      categoryId = categoryId ? categoryId * 1 : undefined
+      if (typeof categoryId === 'number' && categoryId >= 0) {
+        const category = categories.data.find((category) => {
+          return category.id == categoryId
+        })
+
+        return category
+      } else if (categoryId === undefined) {
+        return categories
+      }
+      return false
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async update(categoryId, updatedContent) {
+    try {
+      const { data, metadata } = await this.getCategories()
+      const categoryIndex = data.findIndex(category => category.id == categoryId)
+
+      if (categoryIndex < 0) {
+        return false
+      }
+
+      const category = data[categoryIndex]
+
+      data[categoryIndex] = { ...category, ...updatedContent }
+
+      const dataWriteResponse = await DataFileHandler.writeDataFile('categories', { data, metadata }, 'Category update')
+      console.log(dataWriteResponse);
+
+      return data[categoryIndex]
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async delete(categoryId) {
+    try {
+      const { data, metadata } = await this.getCategories()
+      const categoryIndex = data.findIndex(category => category.id == categoryId)
+
+      if (categoryIndex < 0) {
+        return false
+      }
+
+      const deletedCategory = data.splice(categoryIndex, 1)
+      metadata.totalItems--
+
+      const dataWriteResponse = await DataFileHandler.writeDataFile('categories', { data, metadata }, 'Category delete')
+      console.log(dataWriteResponse);
+
+      return deletedCategory
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = {
+  CategoriesService
+}
---
+++ src/services/users/users.service.js
@@ -0,0 +1,117 @@
+const { getDataPath } = require('@helpers/getDataPath')
+const { DataFileHandler } = require('../../helpers/dataFileHandler')
+const { faker } = require('@faker-js/faker');
+
+class UsersService {
+
+  constructor() {
+    this.usersPath = getDataPath('users')
+  }
+
+  async getUsers () {
+    try {
+      return await DataFileHandler.readDataFile(this.usersPath)
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async create(user) {
+    const cleanBody = {
+      'userName': user.userName,
+      'userAge': user.userAge,
+      'userIdentificationNumber': user.userIdentificationNumber,
+      'userBirthDate': user.userBirthDate,
+      'userBirthCountry': user.userBirthCountry,
+    }
+
+    try {
+      const { data, metadata } = await this.getUsers()
+
+      // identification number must be unique
+      const repeatedIdentificationNumber = data.find(user => user.userIdentificationNumber === cleanBody.userIdentificationNumber)
+      if (repeatedIdentificationNumber)
+        throw new EvalError(`Can't create user because the identification number ${repeatedIdentificationNumber} already exists.`)
+
+      metadata.lastItemId++
+      metadata.totalItems++
+
+      user = { id: metadata.lastItemId, ...cleanBody }
+
+      data.push(user)
+
+      await DataFileHandler.writeDataFile('users', { data, metadata }, 'New user')
+
+      return user
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async find(userId = undefined) {
+    try {
+      const users = await this.getUsers()
+
+      userId = userId ? userId * 1 : undefined
+      if (typeof userId === 'number' && userId >= 0) {
+        const user = users.data.find((user) => {
+          return user.id == userId
+        })
+
+        return user
+      } else if (userId === undefined) {
+        return users
+      }
+      return false
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async update(userId, updatedContent) {
+    try {
+      const { data, metadata } = await this.getUsers()
+      const userIndex = data.findIndex(user => user.id == userId)
+
+      if (userIndex < 0) {
+        return false
+      }
+
+      const user = data[userIndex]
+
+      data[userIndex] = { ...user, ...updatedContent }
+
+      const dataWriteResponse = await DataFileHandler.writeDataFile('users', { data, metadata }, 'User update')
+      console.log(dataWriteResponse);
+
+      return data[userIndex]
+    } catch (err) {
+      throw err
+    }
+  }
+
+  async delete(userId) {
+    try {
+      const { data, metadata } = await this.getUsers()
+      const userIndex = data.findIndex(user => user.id == userId)
+
+      if (userIndex < 0) {
+        return false
+      }
+
+      const deletedUser = data.splice(userIndex, 1)
+      metadata.totalItems--
+
+      const dataWriteResponse = await DataFileHandler.writeDataFile('users', { data, metadata }, 'User delete')
+      console.log(dataWriteResponse);
+
+      return deletedUser
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = {
+  UsersService
+}
