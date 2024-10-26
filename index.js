e9464b72e29541084e969a8fa49a978ca0673537
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2024-10-25 22:26:58 -0500

    🔧 Add CORS validation in index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('module-alias/register')
 const express = require('express');
 const routerApi = require('@routes/index.routes')
+const cors = require('cors');
 const os = require('os');
 const { internalError, handleBoomErrors } = require('@middlewares/errorHandler');
 
@@ -8,9 +9,20 @@ const app = express()
 const port = 8080
 const IP = os.networkInterfaces().eth0[0].address
 
+const originWhitelist = ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (originWhitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error(`${origin} is not authorized to access this application.`), false)
+    }
+  }
+}
+
 // this middleware adds json parsing when Content-Type header specifies application/json
 app.use(express.json())
-
+app.use(cors(corsOptions))
 routerApi(app)
 app.use(handleBoomErrors)
 app.use(internalError)
